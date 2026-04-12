ecb46af0cdd504389e8dfd7b5398441f0d92b6e0
Anatoliy Kolodkin <anatoliy@ankosoftware.com>
2026-04-11 20:40:35 -0500

    feat: add content-addressable hash engine

--- packages/python/agentstack/src/agentstack/hash/__init__.py
+++ packages/python/agentstack/src/agentstack/hash/__init__.py
@@ -1,1 +1,6 @@
 """Content-addressable hash engine for stateless change detection."""
+
+from agentstack.hash.hasher import hash_dict, hash_model
+from agentstack.hash.tree import AgentHashTree, hash_agent
+
+__all__ = ["AgentHashTree", "hash_agent", "hash_dict", "hash_model"]
